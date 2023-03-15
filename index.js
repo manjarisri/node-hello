debd0fa512eaea99b3005f93abced56df1b4d806
Manjari Srivastav <126674100+manjarisri@users.noreply.github.com>
2023-03-15 16:40:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello Jenkins!\n'
+	const msg2 = 'Hello ahdtsurjhe\n'
 	res.end(msg2);
 });
 
