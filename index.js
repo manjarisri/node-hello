9cecd87ae3c97758c96c4761692da433a3c8e64d
Manjari Srivastav <126674100+manjarisri@users.noreply.github.com>
2023-03-16 11:34:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello BAKAA'
+  const msg = 'Hello BAKA'
   res.end(msg);
 });
 
