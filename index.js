95fdd6b05946bfacea2853947f29fdcd26e2b6fb
Manjari Srivastav <126674100+manjarisri@users.noreply.github.com>
2023-07-25 13:13:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// npm install applicationinsights --save
+let appInsights = require("applicationinsights");
+appInsights.setup("[InstrumentationKey=6bbcc05c-064b-4487-a01e-e91dd22423e2;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/]").start();
+
 const http = require('http');
 const port = process.env.PORT || 3000;
 const port2 = process.env.PORT2 || 3001;
